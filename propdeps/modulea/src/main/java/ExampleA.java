e87d2b038766d5b2d744347565e44334d568d915
Phillip Webb <pwebb@vmware.com>
2012-12-05 10:29:59 -0800

    Hacking

---
+++ propdeps/modulea/src/main/java/ExampleA.java
@@ -0,0 +1,11 @@
+
+
+public class ExampleA {
+
+	public static void main(String[] args) {
+		org.apache.commons.lang.ArrayUtils arrayUtils;
+		org.apache.commons.logging.Log log;
+		org.apache.commons.collections.Bag bag;
+	}
+
+}
---
+++ propdeps/modulea/src/test/java/ExampleATest.java
@@ -0,0 +1,12 @@
+
+
+public class ExampleATest {
+
+	public static void main(String[] args) {
+		ExampleA a;
+		org.apache.commons.collections.Bag bag;
+		org.apache.commons.lang.ArrayUtils arrayUtils;
+		org.apache.commons.logging.Log log;
+	}
+
+}
